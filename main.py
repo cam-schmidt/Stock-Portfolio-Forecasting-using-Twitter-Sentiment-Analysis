f3e07a3541bf088b4e5277b652d0803c5724010d
Cam Schmidt <cam_schmidt@outlook.com>
2023-01-11 20:54:59 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 import tweepy
 import re
 import yfinance as yf
-import numpy as np
 import pandas as pd
 from datetime import datetime, time
 import configparser
 from textblob import TextBlob
 from matplotlib import pyplot as plt
 from workalendar.usa import NewYork
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import r2_score
 plt.style.use('fivethirtyeight')
 
 # Read Configs
@@ -27,73 +28,68 @@ auth.set_access_token(access_token, access_token_secret)
 # Make an API object
 api = tweepy.API(auth)
 
-stocks = input('Enter up to 5 ticker symbols, separated by commas: ').upper()
+stocks = input('Enter 5 or more ticker symbols, separated by commas: ').upper()
 stocks = stocks.split(", ")
-stocks = ['$' + x for x in stocks]  # Add cashtag symbol
+stocks = ['$' + x for x in stocks]
 
-#                                  -------------------- Twitter Analysis --------------------
-def twitter_sentiment(stocks):
-    start_date = datetime.now().date()
+start_date = datetime.now().date()
 
-    index = 0
-    search_parameters = {
-        'q': stocks[index],
-        'lang': 'en',
-        'count': 100,
-        'until': start_date,
-        'tweet_mode': 'ext'
-    }
+# Create a list of market holidays, where the stock exchange is closed
+cal = NewYork()
+cal.holidays(2023)
 
-    cursor = tweepy.Cursor(api.search_tweets, **search_parameters)
+# Create a dataframe of stock market holidays
+holiday_df = pd.DataFrame(cal.holidays(2023), columns=['Date', 'Holiday'])
+holiday_df = holiday_df.drop([0, 3, 4, 9, 10, 11, 12])  # Remove non-market-holidays
 
-    # Create a list of market holidays, where the stock exchange is closed
-    cal = NewYork()
-    cal.holidays(2023)
+# Add additional market holidays
+holiday_df.loc[9] = ['2023-04-07', 'Good Friday']
+holiday_df.loc[10] = ['2023-06-19', 'Juneteenth']
 
-    # Create a dataframe of stock market holidays
-    holiday_df = pd.DataFrame(cal.holidays(2023), columns=['Date', 'Holiday'])
-    holiday_df = holiday_df.drop([0, 3, 4, 9, 10, 11, 12])  # Remove non-market-holidays
+holiday_df['Date'] = pd.to_datetime(holiday_df['Date'])
+tickers = []  # empty list to store the ticker of appended tweets
+all_tweets = []  # empty list to store all tweets being used
 
-    # Add additional market holidays
-    holiday_df.loc[9] = ['2023-04-07', 'Good Friday']
-    holiday_df.loc[10] = ['2023-06-19', 'Juneteenth']
 
-    holiday_df['Date'] = pd.to_datetime(holiday_df['Date'])
-    tweets_per_day = 0
-    tickers = []  # empty list to store the ticker of appended tweets
-    all_tweets = []  # empty list to store all tweets being used
-    tweet_iterator = cursor.items(limit=1000)
-    current_day = None
+def retrieve_tweets():
+    global df_x
 
-    for tweet in cursor.items(limit=10000):
-        day = tweet.created_at
-        tweet_date = pd.to_datetime(day.date().strftime('%Y-%m-%d'))
-        # Only add tweets which were made before market open on weekdays
-        if day.time() < time(hour=9, minute=30) and day.weekday() < 5:
-            # Do not add tweets which were made on stock market holidays
-            if not holiday_df['Date'].eq(tweet_date).any():
-                if tweets_per_day < 100:
+    def search_ticker():
+        global stock_search_date
+        tweets_per_day = 0
+        current_day = None
+        for tweet in cursor.items(limit=10000):
+            day = tweet.created_at
+            tweet_date = pd.to_datetime(day.date().strftime('%Y-%m-%d'))
+            # Only add tweets which were made before market open on weekdays
+            if day.time() < time(hour=9, minute=30) and day.weekday() < 5:
+                # Do not add tweets which were made on stock market holidays
+                if not holiday_df['Date'].eq(tweet_date).any():
                     if current_day is None:
-                        current_day = day
-                    if day.date() != current_day.date():  # Only append tweets from the most recent trading day
-                        index += 1
-                        tweets_per_day = 0
+                        current_day = day.date()
+                        stock_search_date = current_day
+                    # Only append tweets from the same trading day
+                    if day.date() != current_day:
                         break
-                    all_tweets.append(tweet.text)
-                    tickers.append(search_parameters['q'])
-                    tweets_per_day += 1
-                    tweet = next(tweet_iterator)
-                elif tweets_per_day == 100:
-                    index += 1
-                    if index >= len(stocks):
+                    if tweets_per_day < 100:
+                        all_tweets.append(tweet.text)
+                        tickers.append(search_parameters['q'])
+                        tweets_per_day += 1
+                    else:
                         break
-                    search_parameters['q'] = stocks[index]  # Update the search query to the next stock
-                    cursor = tweepy.Cursor(api.search_tweets,
-                                           **search_parameters)  # Update the cursor with the new search parameters
-                    tweet_iterator = cursor.items(limit=1000)  # Reset the tweet iterator
-                    tweets_per_day = 0  # Reset the tweets_per_day counter
+        return
 
-    # Create a df to store tweets
+    for stock in stocks:
+        search_parameters = {
+            'q': stock,
+            'lang': 'en',
+            'count': 100,
+            'until': start_date,
+            'tweet_mode': 'ext'
+        }
+        cursor = tweepy.Cursor(api.search_tweets, **search_parameters)
+        search_ticker()
+
     df = pd.DataFrame(all_tweets, columns=['Tweets'])
     df['Ticker'] = tickers
 
@@ -127,12 +123,14 @@ def twitter_sentiment(stocks):
     # Get the average subjectivity and polarity of each ticker's tweets
     df_x = df_x.groupby(df_x['Ticker']).mean()
 
-    return print(df_x)
+    return df_x
 
 
-#                                  -------------------- Stock Analysis --------------------
 def stock_trend(stocks):
+    global df_y
+
     stocks = [s[1:] for s in stocks]  # Remove cashtag from ticker symbols
+
     df_y = pd.DataFrame()
 
     for stock in stocks:
@@ -152,8 +150,40 @@ def stock_trend(stocks):
         # Add the percent change to the DataFrame
         df_y[stock] = [pct_change]
 
-    return print(df_y)
+    return df_y
 
 
-twitter_sentiment(stocks)
-stock_trend(stocks)
+def linear_regression():
+    retrieve_tweets()
+    stock_trend(stocks)
+
+    # Twitter sentiment (polarity) of ticker symbol is the independent variable
+    X = df_x['Polarity'].to_numpy()
+    X = X.reshape(-1, 1)
+
+    # Percentage change of the stock is the dependent variable
+    y = df_y.loc[[0]].to_numpy()
+    y = y.reshape(-1, 1)
+
+    regressor = LinearRegression()
+    regressor.fit(X, y)
+
+    y_pred = regressor.predict(X)
+    r2 = r2_score(y, y_pred)
+
+    # Plot the data points
+    plt.scatter(X, y, color='g')
+
+    # Plot the regression line
+    plt.plot(X, regressor.predict(X), marker='o', color='blue')
+
+    plt.xlabel('Polarity')
+    plt.ylabel('Percentage Change')
+    plt.title('Twitter Sentiment vs Stock Price')
+    text = f"R-Squared = {r2:.3f}"
+    plt.text(1, 1, text, transform=plt.gca().transAxes)
+
+    return plt.show()
+
+
+linear_regression()
